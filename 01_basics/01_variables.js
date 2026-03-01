c73c9cabb4466feb7bc718592e7dbe9184e3b16f
Neelam More <neelammore1404@gmail.com>
2026-03-01 22:50:40 +0000

    let,const,var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+
+let acountemail = "neelam@google.com"
+
+var accountPassword = "12345"
+
+let accountState;
+
+accountCity = "Jaipur"
+
+console.table([accountId , acountemail, accountPassword, accountCity, accountState ])
+
+/**
+ * Prefer not to use var
+ * because of issue in block scope and functional scope
+ */
