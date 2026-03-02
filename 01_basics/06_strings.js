9c60cd50b86afb3eb0d96d16989b296d68789564
Neelam More <neelammore1404@gmail.com>
2026-03-02 02:54:56 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,12 @@
+const name ="neelam"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value") old 
+
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+// string declaration another way
+
+const gameNmae= new String("Game")
+
