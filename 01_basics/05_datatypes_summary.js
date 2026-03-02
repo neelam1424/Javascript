6f4a857059759b47c43defd9316d628078015da2
Neelam More <neelammore1404@gmail.com>
2026-03-02 00:36:02 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,12 @@
+
+//Javascript is dynamic type
+
+// Primitive
+
+// 7 types: String, Number, Boolean, null(empty), undefined, Symbol(make value unique), BigInt
+
+
+// Referenece (non primitive)
+
+
+// Array, * Objects, Functions
